658fd6cb33ee72b1ac253998aa7055da06baaf6f
polyanabittar <polyana.bittar@gmail.com>
2023-12-07 16:55:02 -0300

    deixando os eventos assincronos

--- src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
+++ src/main/kotlin/com/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -3,16 +3,18 @@ package com.mercadolivro.events.listener
 import com.mercadolivro.events.PurchaseEvent
 import com.mercadolivro.service.PurchaseService
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import java.util.UUID
 
 @Component
-class GenerateListener(
+class GenerateNfeListener(
     private val purchaseService: PurchaseService
 ) {
-
+    @Async
     @EventListener
     fun listen(purchaseEvent: PurchaseEvent){
+        println("Gerando nfe da compra")
         val nfe = UUID.randomUUID().toString()
         val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
         purchaseService.update(purchaseModel)
--- src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
+++ src/main/kotlin/com/mercadolivro/service/PurchaseService.kt
@@ -3,6 +3,7 @@ package com.mercadolivro.service
 import com.mercadolivro.events.PurchaseEvent
 import com.mercadolivro.model.PurchaseModel
 import com.mercadolivro.repository.PurchaseRepository
+import jakarta.transaction.Transactional
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.stereotype.Service
 
@@ -11,10 +12,13 @@ class PurchaseService(
     private val purchaseRepository: PurchaseRepository,
     private val applicationEventPublisher: ApplicationEventPublisher
 ) {
+    @Transactional
     fun create(purchaseModel: PurchaseModel) {
         purchaseRepository.save(purchaseModel)
 
+        println("Disparando evento de compra")
         applicationEventPublisher.publishEvent(PurchaseEvent(this, purchaseModel))
+        println("Finalizando processamento")
     }
 
     fun update(purchaseModel: PurchaseModel) {
