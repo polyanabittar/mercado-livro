90664bbc43a631fe867517dc25f4dea22518196d
polyanabittar <polyana.bittar@gmail.com>
2024-04-02 16:23:10 -0300

    test: testando update, delete e email available

--- src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/mercadolivro/service/CustomerServiceTest.kt
@@ -8,9 +8,12 @@ import com.mercadolivro.repository.CustomerRepository
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.SpyK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
@@ -24,6 +27,7 @@ import java.util.Optional
 @ExtendWith(MockKExtension::class)
 class CustomerServiceTest {
     @InjectMockKs
+    @SpyK
     private lateinit var customerService: CustomerService
 
     @MockK
@@ -98,7 +102,7 @@ class CustomerServiceTest {
     }
 
     @Test
-    fun `should fail find customer by id`() {
+    fun `should throw not found exception when find by id`() {
         val id = Random().nextInt()
 
         every { customerRepository.findById(id) } returns Optional.empty()
@@ -111,6 +115,101 @@ class CustomerServiceTest {
         assertEquals("ML-201", error.errorCode)
 
         verify(exactly = 1) { customerRepository.findById(id) }
+    }
+
+    @Test
+    fun `should update customer`() {
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomers(id = id)
+
+        every { customerRepository.existsById(id) } returns true
+        every { customerRepository.save(fakeCustomer) } returns fakeCustomer
+
+        customerService.updateCustomer(fakeCustomer)
+
+        verify(exactly = 1) { customerRepository.existsById(id) }
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    @Test
+    fun `should throw not found exception when update customer`() {
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomers(id = id)
+
+        every { customerRepository.existsById(id) } returns false
+
+        val error = assertThrows<NotFoundException> {
+            customerService.updateCustomer(fakeCustomer)
+        }
+
+        assertEquals("Customer [$id] not exists", error.message)
+        assertEquals("ML-201", error.errorCode)
+
+        verify(exactly = 1) { customerRepository.existsById(id) }
+        verify(exactly = 0) { customerRepository.save(any()) }
+    }
+
+    @Test
+    fun `should delete customer by id`() {
+        val id = Random().nextInt()
+        val fakeCustomer = buildCustomers(id = id)
+        val expectedCustomer = fakeCustomer.copy(status = CustomerStatus.INACTIVE)
+
+        every { customerService.findById(id) } returns fakeCustomer
+        every { bookService.deleteByCustomer(fakeCustomer) } just runs
+        every { customerRepository.save(fakeCustomer) } returns expectedCustomer
+
+        customerService.deleteCustomer(id)
+
+        assertEquals("INACTIVE", fakeCustomer.status.name)
+
+        verify(exactly = 1) { customerService.findById(id) }
+        verify(exactly = 1) { bookService.deleteByCustomer(fakeCustomer) }
+        verify(exactly = 1) { customerRepository.save(expectedCustomer) }
+    }
+
+    @Test
+    fun `should throw not found exception when delete customer by id`() {
+        val id = Random().nextInt()
+
+        every { customerService.findById(id) } throws NotFoundException("Customer [$id] not exists", "ML-201")
+
+        val error = assertThrows<NotFoundException> {
+            customerService.deleteCustomer(id)
+        }
+
+        assertEquals("Customer [$id] not exists", error.message)
+        assertEquals("ML-201", error.errorCode)
+
+        verify(exactly = 1) { customerService.findById(id) }
+        verify(exactly = 0) { bookService.deleteByCustomer(any()) }
+        verify(exactly = 0) { customerRepository.save(any()) }
+    }
+
+    @Test
+    fun `should return false when email is not available`() {
+        val fakeEmail = "${UUID.randomUUID()}@email.com"
+
+        every { customerRepository.existsByEmail(fakeEmail) } returns true
+
+        val existsByEmail = customerService.emailAvailable(fakeEmail)
+
+        assertFalse(existsByEmail)
+
+        verify(exactly = 1) { customerRepository.existsByEmail(fakeEmail) }
+    }
+
+    @Test
+    fun `should return true when email is available`() {
+        val fakeEmail = "${UUID.randomUUID()}@email.com"
+
+        every { customerRepository.existsByEmail(fakeEmail) } returns false
+
+        val existsByEmail = customerService.emailAvailable(fakeEmail)
+
+        assertTrue(existsByEmail)
+
+        verify(exactly = 1) { customerRepository.existsByEmail(fakeEmail) }
     }
 
     private fun buildCustomers(
